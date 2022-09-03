bda3f36e22daec8440294deb96bb39b0f3f672f9
Dar-Na <70232815+Dar-Na@users.noreply.github.com>
2022-09-03 13:50:15 +0200

    Sass fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 
 const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-var replace = require('gulp-replace');
+const replace = require('gulp-replace');
 
 // File paths
 const files = { 
@@ -18,7 +18,7 @@ const files = {
 function scssTask(){    
     return src(files.scssPath)
         .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass([])) // compile SCSS to CSS
+        .pipe(sass({ includePaths: ["node_modules"] })) // compile SCSS to CSS
         .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
         .pipe(dest('dist')
